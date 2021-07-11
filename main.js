545774865af3f24666a8d3fb2ca8de8e8dc2a807
Markos Souza <markossouza255@gmail.com>
2021-07-11 20:40:27 -0300

    adição de responsiviade

--- main.js
+++ main.js
@@ -77,8 +77,35 @@ function backToTop(){
     }
 }
 
+
+/*---Menu visivel conforme a seção visivel na pagina---*/
+const sections = document.querySelectorAll('main section[id]')
+function activeMenuCurrentSection() {
+    const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+    for(const section of sections ) {
+        const sectionTop = section.offsetTop
+        const sectionHeight = section.offsetHeight
+        const sectionId = section.getAttribute('id')
+
+        const checkpointStart = checkpoint >= sectionTop
+        const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+        
+        if(checkpointStart && checkpointEnd) {
+            document.querySelector('nav ul li a[href*=' + sectionId + ']')
+            .classList.add('active')
+            
+        } else {
+            document
+            .querySelector('nav ul li a[href*=' + sectionId + ']')
+            .classList.remove('active')
+        }
+    }
+}
+
 /*----chamadas da mesma função scroll ---*/
 window.addEventListener('scroll', function() {
     backToTop()
     darKengHeader()
+    activeMenuCurrentSection()
 })
